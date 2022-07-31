5742f2c86c5f3db89117f1cbef374d32a0269c65
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2022-07-31 20:30:27 +0200

    added store/restore for gpg keys

--- system-connections/store_system_connections.sh
+++ system-connections/store_system_connections.sh
@@ -7,7 +7,7 @@ ENV_FILE="$REL/../.env"
 TARGET_FILE="$HOME/.encrypted_dotfiles/system-connections.7z"
 [[ -e "$TARGET_FILE" ]] && rm "$TARGET_FILE"
 
-"$REL/../7z/encrypt.sh" -s -i /etc/NetworkManager/system-connections/* -t "$TARGET_FILE" -p "${SYSTEM_CONNECTIONS_PW}"
+"$REL/../7z/encrypt.sh" -s -i "/etc/NetworkManager/system-connections/*" -t "$TARGET_FILE" -p "${SYSTEM_CONNECTIONS_PW}"
 
 sudo chown ""$(whoami)":"$(id -g -n)"" "$TARGET_FILE"
 
