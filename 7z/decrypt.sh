6de2feb7df45bd3d8261507774bfc55f99b9bdc0
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2022-07-31 18:00:50 +0200

    extracted 7z opeation into dedicated scripts

---
+++ 7z/decrypt.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -s|--sudo)
+      SUDO="sudo"
+      shift
+      ;;
+    -p|--password)
+      PASSWORD="$2"
+      shift
+      shift
+      ;;
+    -i|--input)
+      SOURCE="$2"
+      shift
+      shift
+      ;;
+    -t|--target)
+      TARGET="$2"
+      shift
+      shift
+      ;;
+    *)
+      echo "Unknown option $1" >&2
+      exit 1
+  esac
+done
+
+[[ -z "$SOURCE" ]] && echo "-i or --input must be provided" >&2 && exit 1
+[[ -z "$TARGET" ]] && echo "-t or --target must be provided" >&2 && exit 1
+
+$SUDO 7z e -aoa $( [[ -n "$PASSWORD" ]] && echo "-p${PASSWORD}" ) -o"$TARGET" "$SOURCE"
+
--- system-connections/store_system_connections.sh
+++ system-connections/store_system_connections.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-ENV_FILE="$(dirname "$0")/.env"
+REL="$(dirname "$0")"
+ENV_FILE="$REL/../.env"
 [[ -e "$ENV_FILE" ]] && export $(grep -v '^#' "$ENV_FILE" | xargs)
 
 TARGET_FILE="$HOME/.encrypted_dotfiles/system-connections.7z"
 [[ -e "$TARGET_FILE" ]] && rm "$TARGET_FILE"
-sudo 7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on -mhe=on -p"${SYSTEM_CONNECTIONS_PW}" "$TARGET_FILE" /etc/NetworkManager/system-connections/*
+
+"$REL/../7z/encrypt.sh" -s -i /etc/NetworkManager/system-connections/* -t "$TARGET_FILE" -p "${SYSTEM_CONNECTIONS_PW}"
+
 sudo chown ""$(whoami)":"$(id -g -n)"" "$TARGET_FILE"
 
 
